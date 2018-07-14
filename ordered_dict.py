0762525585fbe0efedcb766d56be37bf5e2fe5b1
Craig Rupp <craigrupp@utexas.edu>
2018-07-14 23:05:59 +0000

    All but test_set_repeated_items working

--- ordered_dict.py
+++ ordered_dict.py
@@ -16,8 +16,12 @@ class OrderedDict:
         return coupled_items
     
     def __setitem__(self, key, value):
-        self._keys.append(key)
-        self._values.append(value)
+        if key not in self:
+            self._keys.append(key)
+            self._values.append(value)
+        else:
+            idx = self._keys.index(key)
+            self._values[idx] = value
         
     def __getitem__(self, key):
         for k, v in zip(self._keys, self._values):
@@ -32,26 +36,53 @@ class OrderedDict:
         return len(self.keys())
         
     def __eq__(self, other):
-        if len(self._keys) != len(other._values):
-            return False
-        for k, v in zip(self._keys, self._values):
-            if k not in other._keys or other[k] != v:
+        for key, value in zip(self.keys(), self.values()):
+            if key not in other or other[key] != value:
                 return False
         return True
+        
+    def __ne__(self, other):
+        for i, (a, b) in enumerate(zip(self._keys, self._values)):
+            if a not in other._keys or other._keys[i] != self._keys[i]:
+                return True
+        return False
     
     def __str__(self):
-        result = '{'
-        for key, value in zip(self._keys, self._values):
-            result += '{}: {}, '.format(repr(key), repr(value))
-            result = result.rstrip(', ')
-        result += '}'
-        return result 
+        rst = "{"
+        for key, value in zip(self.keys(), self.values()):
+            rst += "{}: {}, ".format(repr(key), repr(value))
+        rst = rst.rstrip(', ')
+        rst += '}'
+        return rst
+    
     
     #ask for clarity on this
     __repr__ = __str__
     
+    def __add__(self, other):
+        comb_dict = OrderedDict()
+        for ky, vl in zip(self._keys, self._values):
+            if ky in other:
+                continue
+            comb_dict[ky] = vl
+        for k, v in zip(other._keys, other._values):
+            comb_dict[k] = v
+        return comb_dict
         
-    
-    
+    @classmethod
+    def from_keys(cls, seq):
+        new_inst = OrderedDict()
+        for item in seq:
+            new_inst[item] = None
+        return new_inst
+
+d = OrderedDict()
+
+d['language'] = 'Python'
+d['other'] = ('a', 'b', 'c')
+d['language'] = 'Ruby'
+print([d.keys(), d.values()])
+
+
 
 
